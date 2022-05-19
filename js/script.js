eba50b399f114d555e613e02df0d7c6610f1213f
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-19 14:58:31 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -8,42 +8,81 @@ Una volta definita la struttura dati, prendendo come riferimento la card di esem
 
 
 // seleziono gli elementi del DOM
-const team = document.getElementById("team"); 
+let stampaMembro = document.querySelector(".team-container"); 
 
 // creo una lista contenente: nomi, ruoli e foto dei membri
 const aggMembro = [
     {
-        "nomeECognome" : "Angela Caroll",
-        "ruolo" : "Chief Editor"
+        "nomeECognome" : "Wayne Barnett",
+        "ruolo" : "Founder CEO",
+        "img" : "img/wayne-barnett-founder-ceo.jpg"
     },
     {
         "nomeECognome" : "Barbara Ramos",
-        "ruolo" : "Graphic Designer"
+        "ruolo" : "Graphic Designer",
+        "img" : "img/angela-caroll-chief-editor.jpg"
     },
     {
         "nomeECognome" : "Scott Estrada",
-        "ruolo" : "Developer"
+        "ruolo" : "Developer",
+        "img" : "img/scott-estrada-developer.jpg"
     },
     {
         "nomeECognome" : "Walter Gordon",
-        "ruolo" : "Office Manager"
+        "ruolo" : "Office Manager",
+        "img" : "img/walter-gordon-office-manager.jpg"
     },
     {
         "nomeECognome" : "Angela Lopez",
-        "ruolo" : "Social Media Manager"
+        "ruolo" : "Social Media Manager",
+        "img" : "img/angela-lopez-social-media-manager.jpg"
     },
     {
-        "nomeECognome" : "Wayne Barnett",
-        "ruolo" : "Founder CEO"
+        "nomeECognome" : "Angela Caroll",
+        "ruolo" : "Chief Editor",
+        "img" : "img/wayne-barnett-founder-ceo.jpg"
     },  
 ];
-console.log(aggMembro[1]);
-//console.log(aggMembro.nomeECognome);
 
-for (let array in aggMembro){
+//console.log(aggMembro[0]);
+//let stampa = document.querySelector(".team-card").innerHTML += aggMembro;
+
+//for (let array in aggMembro){
     //console.log(array, aggMembro[array]);
-}
+//}
 
+for (let i = 0; i < aggMembro.length; i++){
 
+    // creo la classe "team-card" dove inseriremo il contenuto
+    let areaTeam = document.createElement("div");
+    areaTeam.classList.add("team-card");
+    stampaMembro.append(areaTeam);
 
-//console.log(aggMembro);
+    // creo la classe "card-image" e lo inserisco in "team-card"
+    let cardImgTeam = document.createElement("div");
+    cardImgTeam.classList.add("card-image");
+    areaTeam.append(cardImgTeam);
+
+    // creo il tag "img" da inserire in "card-image"
+    let imgTeam = document.createElement("img");
+    imgTeam.setAttribute("src", aggMembro[i].img);
+    cardImgTeam.append(imgTeam);
+
+    // creo la classe "card-text" per le info
+    let infoTeam = document.createElement("div");
+    infoTeam.classList.add("card-text");
+    areaTeam.append(infoTeam);
+
+    // e inserisco il testo
+    let textInfo = document.createElement("h3");
+    infoTeam.append(textInfo);
+    textTeam = aggMembro[i].nomeECognome;
+    textInfo.append(textTeam);
+
+    let roleTeam = document.createElement("p");
+    infoTeam.append(roleTeam);
+    let textRoleTeam = aggMembro[i].ruolo;
+    roleTeam.append(textRoleTeam);
+    
+ }
+
